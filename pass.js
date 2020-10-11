d51a0bd96131b3ac8cbf547957fea9a7456f412b
Onur <ionurboz@hotmail.com>
2020-10-11 18:51:12 +0300

    Update

--- pass.js
+++ pass.js
@@ -3,9 +3,9 @@
 *** Passed events with Debounce or Throttle.
 *** http://pass-js.github.io
 ***
-***	@version 1.0.1
+*** @version 1.0.1
 ***
-***	@author Onur Boz <hi.bozdev@gmail.com>
+*** @author Onur Boz <hi.bozdev@gmail.com>
 *** @copyright (c) Copyright 2020 Pass JS, Bozdev, Onur Boz, (https://onurboz.com)
 *** @license Released under the MIT License, (https://github.com/bozdev/pass.js/blob/main/LICENSE)
 **/
